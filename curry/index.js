91c8752c913cbcd168d0db4fbccfe620fbf0fcbe
YASWANTH <yashu.vit@gmail.com>
2019-07-24 15:39:14 +0530

    better version of curry

--- curry/index.js
+++ curry/index.js
@@ -1,8 +1,12 @@
 /* 
   1st Iteration:
   Learnings: Took very long time to come up with visualization
+  Approach: Initialized one global outer args to push to that array from all nested Fns
   Time taken: 45mins + for 5 test cases, still 2 of them are failing
 
+
+  2nd Iteration:
+  Approach: Making use of previous clousure
 */
 function curry(fn) {
   var totalArgsCount = fn.length;
@@ -21,12 +25,24 @@ function curry(fn) {
   return innerFn;
 }
 
+function curry1(fn) {
+  var outerArgs = Array.prototype.slice.call(arguments, 1);
+  return function() {
+    var innerArgs = Array.prototype.slice.call(arguments);
+    var totalArgs = outerArgs.concat(innerArgs);
+    if (fn.length === totalArgs.length) {
+      return fn.apply(null, totalArgs);
+    }
+    return curry1.apply(null, [fn, ...totalArgs]);
+  };
+}
+
 function add(a, b) {
   return a + b;
 }
 
-var curried = curry(add);
+var curried = curry1(add);
 console.log(curried(1, 2));
 console.log(curried(1)(2));
 
-module.exports = curry;
+module.exports = curry1;
