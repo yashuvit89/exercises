cf8cb8336405d99cb4dbb68accc0edfc6ef8fcec
YASWANTH <yashu.vit@gmail.com>
2019-07-24 12:06:14 +0530

    debounce

--- debounce/index.js
+++ debounce/index.js
@@ -1,3 +1,32 @@
-function debounce() {
-  console.log("hello");
-}
+/* 
+Learnings
+1. Always clear timer and assign new one, so that new timer starts ticking
+2. Forgot to add (call) Array.prototype.slice.call(arguments)
+*/
+
+var debounce = function(CB, time) {
+  var timerId = null;
+  return function() {
+    clearTimeout(timerId);
+    var fnArgs = Array.prototype.slice.call(arguments);
+    var context = this;
+    timerId = setTimeout(function() {
+      CB.apply(context, fnArgs);
+      clearTimeout(timerId);
+      timerId = null;
+    }, time);
+  };
+};
+
+var callSupplier = function(n) {
+  console.log("this inside supplier");
+};
+
+var debounced = debounce(callSupplier, 2000);
+
+debounced();
+debounced();
+debounced();
+debounced();
+
+module.exports = debounce;
